b64377b6888ff741f7418b2275657e198626df46
Adam Haines <adamlh2003@gmail.com>
2025-11-04 04:58:02 +0000

    Ready code for heroku deployment

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#7w)ti97@p%_x--+$3g_5#)(v(1_^wl*i5t-=waowy-s%a%&3i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
