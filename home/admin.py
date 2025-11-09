2d361fc2012eebe7d2a0c2f3d5b794efe2635de0
Adam Haines <adamlh2003@gmail.com>
2025-11-09 19:49:52 +0000

    Edit admin.py in home to allow admin panel to CRUD models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie, Review, Person, Genre
+
+
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(Person)
+admin.site.register(Genre)
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
