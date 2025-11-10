6b82ea862a5dd3668b2593c42fe92f919a4cedcb
Adam Haines <adamlh2003@gmail.com>
2025-11-10 06:07:46 +0000

    Add comment display functionality

--- home/views.py
+++ home/views.py
@@ -13,9 +13,15 @@ class MovieList(generic.ListView):
 def movie_detail(request, slug):
     queryset = Movie.objects.filter(status=1)
     movie = get_object_or_404(queryset, slug=slug)
+    reviews = movie.reviews.all().order_by("-created_on")
+    review_count = movie.reviews.filter(approved=True).count()
 
     return render(
         request,
         "home/movie_detail.html",
-        {"movie": movie},
+        {
+        "movie": movie,
+        "reviews": reviews,
+        "review_count": review_count,
+    },
     )
