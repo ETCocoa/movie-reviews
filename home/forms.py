6aa48d8a267d3fe50924d2e77585f17bea6320f6
Adam Haines <adamlh2003@gmail.com>
2025-11-10 07:10:23 +0000

    Add edit functionality to reviews

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,15 @@ from django import forms
 
 
 class ReviewForm(forms.ModelForm):
+    rating = forms.ChoiceField(
+        choices=[(i, i) for i in range(1, 11)],
+        widget=forms.Select
+    )
+    body = forms.CharField(
+        required=False,
+        label="Comment (optional)"
+    )
+
     class Meta:
         model = Review
-        fields = ('body',)
+        fields = ('body', 'rating')
--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.MovieList.as_view(), name="home"),
     path('<slug:slug>/', views.movie_detail, name='movie_detail'),
+    path('<slug:slug>/edit_review/<int:review_id>',
+         views.comment_edit, name='comment_edit'),
 ]
