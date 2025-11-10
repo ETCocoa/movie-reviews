57521fad2730afb2a9ca40047b33d080b52d7b3c
Adam Haines <adamlh2003@gmail.com>
2025-11-10 02:25:12 +0000

    Add richtext to admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Movie, Review, Person, Genre
+from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(Movie)
+class MovieAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'released')
+    search_fields = ['title', 'description']
+    list_filter = ('status', 'released',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
 
 
 # Register your models here.
-admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(Person)
 admin.site.register(Genre)
